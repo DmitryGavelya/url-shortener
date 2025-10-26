98debe1cf43a68e9e59765caca547ca71da2d22b
Dmitry-Gavelya <dimagavelya7@gmail.com>
2025-10-26 16:21:10 +0300

    Updates

--- static/js/488.d53cbc4a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkurl_shortener_frontend=self.webpackChunkurl_shortener_frontend||[]).push([[488],{488:(e,n,r)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.d53cbc4a.chunk.js.map
---
+++ static/js/488.e10f1faf.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkurl_shortener=self.webpackChunkurl_shortener||[]).push([[488],{488:(e,r,s)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.e10f1faf.chunk.js.map
